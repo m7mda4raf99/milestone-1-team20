49c5b9b0cd76c1d0bbcd9e26d1837782899b8c09
Mohamed Ashraf <62084282+m7mda4raf99@users.noreply.github.com>
2020-12-18 00:56:45 +0200

    Update Department.js

--- models/Department.js
+++ models/Department.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const departmentSchema = new mongoose.Schema({
     name :{ 
         type: String,
+    },
+    faculty_name:{
+        type: String
     }
    
 })
