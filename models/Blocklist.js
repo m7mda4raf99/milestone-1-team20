4153a917f094e6463f46e50e447cb1a1fb621880
Mohamed Ashraf <62084282+m7mda4raf99@users.noreply.github.com>
2020-12-25 19:10:59 +0200

    Last Last b2a one :'((

--- models/Blocklist.js
+++ models/Blocklist.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose= require('mongoose')
 
 const blocklist=new mongoose.Schema({
@@ -9,15 +8,3 @@ const blocklist=new mongoose.Schema({
 )
 
 module.exports = mongoose.model('Blocklist', blocklist)
-=======
-
-const mongoose = require('mongoose')
-
-const blocklist = new mongoose.Schema({
-    name: {
-        type: String
-    }
-})
-   
-module.exports = mongoose.model('Blocklist', blocklist)
->>>>>>> a11f25cf9b3627282783a8f5ac47d51b45bdabbc
